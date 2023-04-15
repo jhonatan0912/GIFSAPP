7e4896c8efff93faa5a44fd4fb63afbede545e63
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-04-15 10:07:19 -0500

    implements saveLocalStorage method in gifs service

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
@@ -7,34 +7,33 @@ import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 })
 export class GifsService {
 
-  constructor(private http: HttpClient) {
-  }
-
   private apiKey: string = 'sFPUjFWUFrB4Vk0us9ORhJwKsuPdWspO'
   private _historial: string[] = [];
 
   public resultados: Gif[] = [];
+
+  private http = inject(HttpClient);
 
   get historial() {
     return [...this._historial]
   }
 
   buscarGifs(tag: string = '') {
-
     tag = tag.trim().toLocaleLowerCase();
-
     if (!this._historial.includes(tag)) {
       this._historial.unshift(tag);
       this._historial = this._historial.splice(0, 10);
     }
-
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=sFPUjFWUFrB4Vk0us9ORhJwKsuPdWspO&q=${tag}&limit=10`)
       .subscribe((response) => {
         console.log(response.data);
         this.resultados = response.data
-      })
+        this.saveLocalStorage();
+      });
+  }
 
-    console.log(this._historial);
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._historial));
   }
 
 }
